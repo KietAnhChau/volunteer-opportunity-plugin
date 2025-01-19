1bde7c1c53554500bd0cd580e59b74a600edc587
Kiet Chau <kiet.chau@mohawkcollege.ca>
2025-01-18 23:23:07 -0500

    Implement activation, deactivation, and uninstall hooks for the Volunteer Opportunity Plugin

--- volunteer-opportunity-plugin.php
+++ volunteer-opportunity-plugin.php
@@ -1,11 +1,38 @@
 <?php
 
 /**
- * Plugin Name: Volunteer Opportunity Plugin
- * Description: A plugin for Volunteer Opportunity.
- * Author: Kiet Anh Chau
- * Version: 1.0
- * 
- */
+* Plugin Name: Volunteer Opportunity Plugin
+* Description: A plugin for Volunteer Opportunity.
+* Author: Kiet Anh Chau
+* Version: 1.0
+* 
+*/
+
+
+
+function plugin_activate()
+{
+
+}
+
+function plugin_deactivate()
+{
+
+}
+
+
+function plugin_uninstall()
+{
+
+}
+
+
+
+
+// Register Custom Hooks
+register_activation_hook(__FILE__, "plugin_activate");
+register_deactivation_hook(__FILE__, "plugin_deactivate");
+register_uninstall_hook(__FILE__, "plugin_uninstall");
+
 
 ?>
